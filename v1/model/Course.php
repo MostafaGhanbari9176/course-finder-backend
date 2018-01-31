6d26567478313801755a3616bf624d7edd48b3d6
M_gh <godhelot1@outlook.com>
2018-01-31 15:35:53 +0330

    Course And Upload

--- v1/model/Course.php
+++ v1/model/Course.php
@@ -11,6 +11,7 @@ class Course
 {
     private $conn;
     private $tablename = 'cource';
+
     public function __construct()
     {
         $db = new DBConnection();
@@ -19,56 +20,72 @@ class Course
         mysqli_set_charset($this->conn, "UTF8");
     }
 
-    public function addCourse($teacher_id, $subject, $tabaghe_id, $type, $capacity, $mony, $sharayet, $tozihat, $start_date, $end_date, $day, $hours, $old_range){
+    public function addCourse($teacher_id, $subject, $tabaghe_id, $type, $capacity, $mony, $sharayet, $tozihat, $start_date, $end_date, $day, $hours, $old_range)
+    {
         $sql = "INSERT INTO $this->tablename (teacher_id, subject, tabaghe_id, type, capacity, mony, sharayet, tozihat, start_date, end_date, day, hours, old_range) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
         $rezult = $this->conn->prepare($sql);
-        $rezult->bind_param('ssiiiissssssi',$teacher_id, $subject, $tabaghe_id, $type, $capacity, $mony, $sharayet, $tozihat, $start_date, $end_date, $day, $hours, $old_range);
-        if($rezult->execute())
-            return 1;
-        return 0;
+        $rezult->bind_param('ssiiiissssssi', $teacher_id, $subject, $tabaghe_id, $type, $capacity, $mony, $sharayet, $tozihat, $start_date, $end_date, $day, $hours, $old_range);
+        if ($rezult->execute()) {
+            $sql2 = "SELECT c.cource_id FROM  $this->tablename c WHERE c.teacher_id = ?";
+            $rezult2 = $this->conn->prepare($sql2);
+            $rezult2->bind_param('s', $teacher_id);
+            $rezult2->execute();
+            $rezult2 = $rezult2->get_result();
+            $res = 0;
+            while ($row = $rezult2->fetch_assoc()) {
+                $res = $row['cource_id'];
+            }
+            return $res;
+        }
+        return (int)0;
     }
 
-    public function getAllCourse(){
+    public function getAllCourse()
+    {
 
         $sql = "SELECT * FROM $this->tablename";
         $result = $this->conn->prepare($sql);
-        if($result->execute())
+        if ($result->execute())
             return $result->get_result();
         return 0;
     }
 
-    public function getCourseById($id){
+    public function getCourseById($id)
+    {
         $sql = "SELECT * FROM $this->tablename c WHERE c.cource_id = ?";
         $result = $this->conn->prepare($sql);
-        $result->bind_param('i',$id);
-        if($result->execute())
+        $result->bind_param('i', $id);
+        if ($result->execute())
             return $result->get_result();
         return 0;
     }
 
-    public function getCourseByTeacherId($teacherId){
+    public function getCourseByTeacherId($teacherId)
+    {
         $sql = "SELECT * FROM $this->tablename c WHERE c.teacher_id = ?";
         $result = $this->conn->prepare($sql);
-        $result->bind_param('i',$teacherId);
-        if($result->execute())
+        $result->bind_param('i', $teacherId);
+        if ($result->execute())
             return $result->get_result();
         return 0;
     }
 
-    public function getTeacherSubject($id){
+    public function getTeacherSubject($id)
+    {
         $sql = "SELECT t.subject FROM teacher t WHERE t.phone = (SELECT c.teacher_id FROM $this->tablename c WHERE c.cource_id = ?)";
         $resuelt = $this->conn->prepare($sql);
-        $resuelt->bind_param('i',$id);
-        if($resuelt->execute())
+        $resuelt->bind_param('i', $id);
+        if ($resuelt->execute())
             return $resuelt->get_result()->fetch_assoc()['subject'];
         return 0;
     }
 
-    public function getTabaghe($id){
+    public function getTabaghe($id)
+    {
         $sql = "SELECT t.subject FROM tabaghe t WHERE t.id = (SELECT c.tabaghe_id FROM $this->tablename c WHERE c.cource_id = ?)";
         $resuelt = $this->conn->prepare($sql);
-        $resuelt->bind_param('i',$id);
-        if($resuelt->execute())
+        $resuelt->bind_param('i', $id);
+        if ($resuelt->execute())
             return $resuelt->get_result()->fetch_assoc()['subject'];
         return 0;
     }
