b5b0b47bd74d0e16bcef84e6e18581c26ea0ce8c
M_gh <godhelot1@outlook.com>
2018-06-27 14:04:57 +0330

    improve

---
+++ v1/WV/index.php
@@ -0,0 +1,43 @@
+﻿<html>
+<head>
+    <script>
+        function myFunction(elementName) {
+            /* Get the text field */
+            var copyText = document.getElementById(elementName);
+
+            /* Select the text field */
+            copyText.select();
+
+            /* Copy the text inside the text field */
+            document.execCommand("copy");
+
+            /* Alert the copied text */
+            alert("Copied the text: " + copyText.value);
+        }
+    </script>
+
+    <link><link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body >
+
+<table style="width:100%">
+    <tr style="direction:inherit ">
+        <th>کد هدیه</th>
+        <th>تعداد باقی مانده</th>
+    </tr>
+    <tr style="margin: 20px">
+        <td><input type="text" value="abcd" id="myInput1">
+            <button onclick="myFunction('myInput1')">Copy text</button></td>
+        <td>20</td>
+
+    </tr>
+    <tr >
+        <td ><input type="text" value="fetr97" id="myInput2">
+            <button onclick="myFunction('myInput2')">Copy text</button></td>
+        <td>10</td>
+    </tr>
+</table>
+
+
+</body>
+</html>
