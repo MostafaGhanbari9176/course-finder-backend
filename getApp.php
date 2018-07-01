b9ee05334cb4afc4276418d841517a3c30ac9649
M-gh <godhelot1@outlook.com>
2018-07-01 06:46:33 +0430

    improve

--- getApp.php
+++ getApp.php
@@ -2,7 +2,7 @@
 header("Content-type:application/apk");
 
 // It will be called downloaded.pdf
-header("Content-Disposition:attachment;filename='CourseFinder.apk'");
+header("Content-Disposition:attachment;filename='دوره یاب.apk'");
 
 // The PDF source is in original.pdf
 readfile("apk/CourseFinder.apk");
